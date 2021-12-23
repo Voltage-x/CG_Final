f5d88d726b12b329470dd63316ea23ac894fe157
DESKTOP-VOMDDOA\Vincent <karta1110257@gmail.com>
2021-12-23 14:46:57 +0800

    1223

---
+++ compare1.py
@@ -0,0 +1,29 @@
+# In[1]:
+import numpy as np
+import perfplot
+
+
+def compare(n):
+    np.where((n > 0) & (n < n), n, np.inf)
+
+
+def if_cond(n):
+    compareL = np.zeros(np.shape(n))
+    for index in range(len(n)-1):
+        if n[index] > 0:
+            compareL[index] = min(n[index], n[index+1])
+        else:
+            compareL[index] = np.inf
+
+
+b = perfplot.bench(
+    equality_check=None,
+    setup=lambda n: np.random.rand(n),
+    kernels=[compare, if_cond],
+    labels=["numpy.where()", "if condition + min()"],
+    n_range=[2 ** k for k in range(25)],
+    xlabel="len(a)",
+)
+b.show()
+
+# %%
---
+++ compare2.py
@@ -0,0 +1,29 @@
+# In[1]:
+import numpy as np
+import perfplot
+from functools import reduce
+
+
+def compare(n):
+    compareL = np.full(np.shape(n), np.inf)
+    for i in range(4):
+        for index in range(len(n)):
+            if n[index] < compareL[index]:
+                compareL[index] = n[index]
+
+
+def compare_num(n):
+    reduce(np.minimum, [n, n, n, n])
+
+
+b = perfplot.bench(
+    equality_check=None,
+    setup=lambda n: np.random.rand(n),
+    kernels=[compare_num, compare],
+    labels=["numpy.minimum() + reduce", "if condition + for loop"],
+    n_range=[2 ** k for k in range(25)],
+    xlabel="len(a)",
+)
+b.show()
+
+# %%
